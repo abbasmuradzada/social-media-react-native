2749810724d6968c041f22d3b552315b58089222
Abbas Muradzada <abbas.muradzada@cybetnet.az>
2021-08-07 21:43:13 +0400

    get all posts was connected

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,9 @@
 import React, { useContext, useState, useEffect } from 'react'
-import { Text, Button } from 'react-native-elements'
-import { FlatList, View } from 'react-native'
+import { Text, Button, Card, Badge } from 'react-native-elements'
+import { FlatList, View, Image } from 'react-native'
 import Spacer from '../components/Spacer'
 import { Context as AuthContext } from '../context/AuthContext'
-import authApi from '../services/auth'
+import moment from 'moment'
 import globalService from '../services/auth'
 
 const Home = () => {
@@ -17,10 +17,7 @@ const Home = () => {
                 console.log(res.data.posts);
             })
             .catch((err) => {
-                console.log('==========err==============');
                 console.log(err);
-                console.log('========================');
-
             });
     }, [])
 
@@ -35,13 +32,19 @@ const Home = () => {
             <Spacer>
                 <Button title='Log Out' onPress={() => logout()} />
             </Spacer>
-            <Spacer>
-                <FlatList keyExtractor={item => item.content} data={posts} renderItem={({ item }) => 
-                <View>
-                    <Text>{item.content}</Text>
-                    <Text>{item.createdAt}</Text>
-                </View>} />
-            </Spacer>
+            {/* <Spacer> */}
+            <FlatList keyExtractor={item => item._id} data={posts} renderItem={({ item }) =>
+                <Card>
+                    <Card.Title>{item.postedUser[0].userName}</Card.Title>
+                    <Card.Divider />
+                    <Image source={{ uri: item.postedUser[0].profilePicture }} />
+                    <Text style={{ marginBottom: 16 }}>{item.content}</Text>
+                    <Text><Badge status='error' value={item.likesCount ?? 0} /> Like</Text>
+                    <Text><Badge status='warning' value={item.commentCount ?? 0} /> Comment</Text>
+                    <Card.Divider />
+                    <Text style={{ color: 'gray' }}>{moment(item.createdAt).fromNow()}</Text>
+                </Card>} />
+            {/* </Spacer> */}
         </>
     )
 }
